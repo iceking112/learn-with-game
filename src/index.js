0928f41a7957a274a7fece85b814d25b44b92eaf
zzh <zhangzhh@haoyitec.cn>
2017-06-27 16:43:37 +0800

    [Optimize]添加createLoading，修改route为动态加载

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import dva from 'dva';
+import { browserHistory } from 'dva/router';
+import createLoading from 'dva-loading';
+import { message } from 'antd';
 import './index.css';
 
 // 1. Initialize
-const app = dva();
-
-app.model(require("./models/dayTask"));
+const app = dva({
+  history: browserHistory,
+  onError(e) {
+    message.error(e.message, ERROR_MSG_DURATION);
+  },
+});
 
 // 2. Plugins
-// app.use({});
+app.use(createLoading());
 
 // 3. Model
-// app.model(require('./models/example'));
+// Moved to router.js
 
 // 4. Router
 app.router(require('./router'));
--- src/router.js
+++ src/router.js
@@ -1,16 +1,38 @@
 import React from 'react';
-import { Router, Route } from 'dva/router';
-import IndexPage from './routes/IndexPage';
+import { Router } from 'dva/router';
 
-import DayTask from "./routes/DayTask.js";
+const cached = {};
+function registerModel(app, model) {
+  if (!cached[model.namespace]) {
+    app.model(model);
+    cached[model.namespace] = 1;
+  }
+}
 
-function RouterConfig({ history }) {
-  return (
-    <Router history={history}>
-      <Route path="/" component={IndexPage} />
-      <Route path="/dayTask" component={DayTask} />
-    </Router>
-  );
+function RouterConfig({ history, app }) {
+  const routes = [
+    {
+      path: '/',
+      name: 'IndexPage',
+      getComponent(nextState, cb) {
+        require.ensure([], (require) => {
+          cb(null, require('./routes/IndexPage'));
+        });
+      },
+    },
+    {
+      path: '/dayTask',
+      name: 'DayTask',
+      getComponent(nextState, cb) {
+        require.ensure([], (require) => {
+          registerModel(app, require('./models/dayTask'));
+          cb(null, require('./routes/DayTask'));
+        });
+      },
+    },
+  ];
+
+  return <Router history={history} routes={routes} />;
 }
 
 export default RouterConfig;
