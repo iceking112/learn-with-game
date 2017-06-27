0eb3358b550c80a1e3cffafef93bf5bf34cbb96f
zzh <zhangzhh@haoyitec.cn>
2017-06-27 16:44:49 +0800

    [Optimize]完善每日任务

--- src/components/DayTask/TaskCard.js
+++ src/components/DayTask/TaskCard.js
@@ -6,9 +6,9 @@ function TaskCard({title, taskList, onChange}) {
   return (
     <div className={styles.normal}>
       <Card title={title} extra={<a href="#">添加</a>} style={{ width: 300 }}>
-        <Checkbox onChange={onChange}>Checkbox</Checkbox>
-        <Checkbox onChange={onChange}>Checkbox</Checkbox>
-        <Checkbox onChange={onChange}>Checkbox</Checkbox>
+        <Checkbox className={styles.taskList} onChange={onChange}>Checkbox</Checkbox>
+        <Checkbox className={styles.taskList} onChange={onChange}>Checkbox</Checkbox>
+        <Checkbox className={styles.taskList} onChange={onChange}>Checkbox</Checkbox>
       </Card>
     </div>
   );
--- src/routes/DayTask.js
+++ src/routes/DayTask.js
@@ -3,19 +3,21 @@ import { connect } from 'dva';
 import styles from './DayTask.css';
 import { Calendar } from 'antd';
 import TaskCard from 'components/DayTask/TaskCard'
-
-function DayTask() {
+import MainLayout from 'components/MainLayout/MainLayout';
+function DayTask({ location }) {
   const onSelect = (date) => {
     console.log(date);
   }
 
   return (
-    <div className={styles.normal}>
-      <div style={{ width: 290, border: '1px solid #d9d9d9', borderRadius: 4 }}>
-        <Calendar fullscreen={false} onSelect={onSelect} />
-        <TaskCard title="重要且紧急"></TaskCard>
+    <MainLayout location={location}>
+      <div className={styles.normal}>
+        <div style={{ width: 290, border: '1px solid #d9d9d9', borderRadius: 4 }}>
+          <Calendar fullscreen={false} onSelect={onSelect} />
+          <TaskCard title="重要且紧急"></TaskCard>
+        </div>
       </div>
-    </div>
+    </MainLayout>
   );
 }
 
